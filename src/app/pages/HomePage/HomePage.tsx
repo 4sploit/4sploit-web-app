178574aac816d762dacd552930c6d8820a2548ef
4sploit <4splo17@gmail.com>
2022-04-11 02:46:17 +0300

    remove gh-pages

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
-  return <>Home Page</>;
+  return <>
+    Home Page
+    <Link to={"/projects"}>projects</Link>
+  </>;
 };
 
 export default HomePage;
