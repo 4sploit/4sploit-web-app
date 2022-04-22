59a22c3545cc107c088e713705e7a87b7e7aca40
4sploit <4splo17@gmail.com>
2022-04-22 18:40:20 +0300

    import styled component in component template

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Props, defaultProps } from "./TemplateName.props";
+import { StyledTemplateName } from "./TemplateName.style";
 
 const TemplateName: FC<Props> = (props: Props) => {
   return (
