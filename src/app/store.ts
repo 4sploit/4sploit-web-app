e6473281ad33a4296a293728af38cea40c4af4df
4sploit <4splo17@gmail.com>
2022-05-01 01:18:39 +0300

    disable redux devtools support in production env

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { env } from "src/config";
 import rootReducer from "./rootReducer";
 
 export const store = configureStore({
   reducer: rootReducer,
+  devTools: env.environment !== "production",
 });
 
 export type AppDispatch = typeof store.dispatch;
