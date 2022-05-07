691a3911dc2ecd575b20220179ff865230cc7ab6
4sploit <4splo17@gmail.com>
2022-05-08 02:22:46 +0300

    move footer outer styling to app layout

--- src/common/layouts/AppLayout/AppLayout.style.ts
+++ src/common/layouts/AppLayout/AppLayout.style.ts
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import { Sizes } from "common/styles";
 import styled from "styled-components";
 
 interface Props {}
@@ -7,17 +8,26 @@ export const Wrapper = styled.div<Props>`
   display: flex;
   flex-direction: column;
   background-color: #404040;
+  min-height: 100vh;
 `;
 
 export const HeaderContainer = styled(Container)<Props>``;
 
 export const MainContainer = styled(Container)<Props>`
-  height: 90vh;
-  margin-top: 4.5rem;
-  max-width: none;
+  margin-top: 4rem;
+  word-break: break-word;
+  max-width: ${({ theme }) =>
+    theme.typography.pxToRem(theme.breakpoints.values.desktop)};
+  ${({ theme }) => theme.breakpoints.up(Sizes.Tablet)} {
+    margin-top: 6rem;
+  }
 `;
 
 export const FooterContainer = styled(Container)<Props>`
-  height: 4.5vh;
-  max-width: none;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: #272727;
+  box-shadow: 0px -2px 4px -1px rgb(0 0 0 / 20%),
+    0px -4px 5px 0px rgb(0 0 0 / 14%), 0px -1px 10px 0px rgb(0 0 0 / 12%);
 `;
--- src/features/Footer/Footer.style.ts
+++ src/features/Footer/Footer.style.ts
@@ -1,26 +1,13 @@
 import { SvgIcon } from "@mui/material";
 import { Link } from "common/components";
-import { Sizes } from "common/styles";
 import styled from "styled-components";
 
 interface Props {}
 
-export const StyledFooter = styled.footer<Props>`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background-color: #272727;
-  box-shadow: 0px -2px 4px -1px rgb(0 0 0 / 20%),
-    0px -4px 5px 0px rgb(0 0 0 / 14%), 0px -1px 10px 0px rgb(0 0 0 / 12%);
-`;
+export const StyledFooter = styled.footer<Props>``;
 
 export const Container = styled.div<Props>`
   display: flex;
-  padding-left: 0.2rem;
-  ${({ theme }) => theme.breakpoints.up(Sizes.Tablet)} {
-    padding-left: 1rem;
-  }
 `;
 
 export const ExternalLinksContainer = styled.div<Props>`
