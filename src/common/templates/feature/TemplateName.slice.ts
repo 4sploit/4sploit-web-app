bd8cfc733f8031c9ee7a690bb8a16fb80a19372a
4sploit <4splo17@gmail.com>
2022-05-10 23:58:43 +0300

    add slice template

---
+++ src/common/templates/feature/TemplateName.slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const TemplateNameSlice = createSlice({
+  name: "TemplateName",
+  initialState,
+  reducers: {},
+});
+
+export default TemplateNameSlice.reducer;
