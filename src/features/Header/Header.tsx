7adeb22f80e35fd23710b21310ba976e3953dbd0
4sploit <4splo17@gmail.com>
2022-05-12 02:54:50 +0300

    access static icons by dot operator

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -3,16 +3,15 @@ import { useAppSelector } from "app/hooks";
 import { NavBar } from "common/components/NavBar";
 import { HeaderProps, defaultProps } from "./Header.props";
 import { StyledHeader } from "./Header.style";
-import { IconNames, Icons } from "common/globals";
+import { Icons } from "common/globals";
 
 const Header: FC<HeaderProps> = () => {
   const headerStore = useAppSelector((state) => state.header);
   const links = headerStore.links;
-  const logo = Icons[IconNames.Logo];
 
   return (
     <StyledHeader>
-      <NavBar Logo={logo} links={links} />
+      <NavBar Logo={Icons.LOGO} links={links} />
     </StyledHeader>
   );
 };
