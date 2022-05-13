2d15a60ff14c7ad40a8ebdd297b8d5e8e19cee6f
4sploit <4splo17@gmail.com>
2022-05-14 00:47:10 +0300

    not found page implementation

--- src/pages/NotFoundPage/NotFoundPage.style.ts
+++ src/pages/NotFoundPage/NotFoundPage.style.ts
@@ -1,5 +1,44 @@
-import styled from "styled-components";
+import { SvgIcon } from "@mui/material";
+import { Button, Link } from "common/components";
+import styled, { css } from "styled-components";
 
-interface StyledNotFoundPageProps {}
+interface Props {}
 
-export const StyledNotFoundPage = styled.div<StyledNotFoundPageProps>``;
+const StyledSvgIcon = styled(SvgIcon)<Props>`
+  font-size: ${({ fontSize }) => `${fontSize}rem`};
+`;
+
+const SharedContainerStyle = css<Props>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Container = styled.div<Props>`
+  ${SharedContainerStyle}
+`;
+
+export const ContentContainer = styled.div<Props>`
+  ${SharedContainerStyle}
+`;
+
+export const LinksContainer = styled.div<Props>`
+  display: flex;
+  column-gap: 2rem;
+  padding: 1rem;
+  flex-wrap: wrap;
+`;
+
+export const TitleIcon = styled(StyledSvgIcon)<Props>``;
+
+export const Title = styled.h1<Props>``;
+
+export const HomePageLink = styled(Link)<Props>`
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export const HomePageLinkIcon = styled(StyledSvgIcon)<Props>``;
+
+export const GoBackButton = styled(Button)<Props>``;
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,11 +2,42 @@ import { FC } from "react";
 import { withPageMetadata } from "common/hocs";
 import { metadata } from "./NotFoundPage.metadata";
 import { NotFoundPageProps, defaultProps } from "./NotFoundPage.props";
-import { StyledNotFoundPage } from "./NotFoundPage.style";
+import {
+  Container,
+  ContentContainer,
+  GoBackButton,
+  HomePageLink,
+  HomePageLinkIcon,
+  LinksContainer,
+  Title,
+  TitleIcon,
+} from "./NotFoundPage.style";
+import { useNavigate } from "react-router-dom";
+import { Actions, Icons, Variants } from "common/globals";
 
-const NotFoundPage: FC<NotFoundPageProps> = (props: NotFoundPageProps) => {
+const NotFoundPage: FC<NotFoundPageProps> = () => {
+  const navigate = useNavigate();
+  const ErrorIcon = Icons.ERROR;
+  const HomeIcon = Icons.HOME;
+
   return (
-    <StyledNotFoundPage {...props}>Page Not Found</StyledNotFoundPage>
+    <Container>
+      <ContentContainer>
+        <TitleIcon fontSize="8" as={ErrorIcon} />
+        <Title>Page Not Found</Title>
+      </ContentContainer>
+      <LinksContainer>
+        <HomePageLink url="/">
+          <HomePageLinkIcon fontSize="2.5" as={HomeIcon} />
+        </HomePageLink>
+        <GoBackButton
+          variant={Variants.Contained}
+          action={Actions.Primary}
+          onClick={() => navigate(-1)}>
+          Go Back
+        </GoBackButton>
+      </LinksContainer>
+    </Container>
   );
 };
 
