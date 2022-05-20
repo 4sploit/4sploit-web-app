9f613d4b5c7184c09c9dd4deeb5a359353747e1a
4sploit <4splo17@gmail.com>
2022-05-20 12:59:28 +0300

    import link model

--- src/features/Header/Header.slice.ts
+++ src/features/Header/Header.slice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { IconNames } from "common/globals";
-import { Link } from "common/types";
+import { Link } from "common/models";
 
 interface Header {
   logo: IconNames;
