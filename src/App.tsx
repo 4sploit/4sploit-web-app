a5be984c08b63770a2549bee45e9440cc341f892
4sploit <4splo17@gmail.com>
2022-05-27 19:57:57 +0300

    remove inversify react provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,32 @@ import { ThemeProvider } from "styled-components";
 import { appRoutes, comingSoonRoute } from "./App.routes";
 import { HelmetProvider } from "react-helmet-async";
 import { AppTheme, GlobalStyle } from "common/styles";
-import { env, ioContainer } from "config";
+import { env } from "config";
 import { Provider as ReduxProvider } from "react-redux";
 import { PageMetadata } from "features/PageMetadata";
 import { CssBaseline, StyledEngineProvider } from "@mui/material";
 import { store } from "store";
-import { Provider as InversifyProvider } from "inversify-react";
 
 const App = () => {
   return (
-    <InversifyProvider container={ioContainer}>
-      <ReduxProvider store={store}>
-        <HelmetProvider>
-          <PageMetadata />
-          <StyledEngineProvider injectFirst>
-            <MuiThemeProvider theme={AppTheme}>
-              <ThemeProvider theme={AppTheme}>
-                <CssBaseline />
-                <GlobalStyle />
-                <BrowserRouter>
-                  <Routes>
-                    {env.isUnderConstruction ? comingSoonRoute : appRoutes}
-                  </Routes>
-                </BrowserRouter>
-              </ThemeProvider>
-            </MuiThemeProvider>
-          </StyledEngineProvider>
-        </HelmetProvider>
-      </ReduxProvider>
-    </InversifyProvider>
+    <ReduxProvider store={store}>
+      <HelmetProvider>
+        <PageMetadata />
+        <StyledEngineProvider injectFirst>
+          <MuiThemeProvider theme={AppTheme}>
+            <ThemeProvider theme={AppTheme}>
+              <CssBaseline />
+              <GlobalStyle />
+              <BrowserRouter>
+                <Routes>
+                  {env.isUnderConstruction ? comingSoonRoute : appRoutes}
+                </Routes>
+              </BrowserRouter>
+            </ThemeProvider>
+          </MuiThemeProvider>
+        </StyledEngineProvider>
+      </HelmetProvider>
+    </ReduxProvider>
   );
 };
 
