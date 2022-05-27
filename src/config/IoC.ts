d900fc7de0bf006a4a54d739e6b06db48fc45d5b
4sploit <4splo17@gmail.com>
2022-05-27 20:06:30 +0300

    init bindings and decorations in ioc config file

--- src/config/IoC.ts
+++ src/config/IoC.ts
@@ -1,5 +1,27 @@
-import { Container } from "inversify";
+import "reflect-metadata";
+import HeaderApi from "features/Header/Header.api";
+import { ApiClient } from "common/clients";
+import { Container, decorate, inject, injectable } from "inversify";
 
-const IoContainer = new Container();
+export enum TypeNames {
+  ApiClient = "ApiClient",
+  HeaderApi = "HeaderApi",
+}
 
-export default IoContainer;
+export const Types: Record<TypeNames, symbol> = {
+  [TypeNames.ApiClient]: Symbol.for("ApiClient"),
+  [TypeNames.HeaderApi]: Symbol.for("HeaderApi"),
+};
+
+const ioContainer = new Container({ defaultScope: "Singleton" });
+
+decorate(injectable(), ApiClient);
+decorate(injectable(), HeaderApi);
+decorate(inject<ApiClient>(Types.ApiClient), HeaderApi, 0);
+
+ioContainer
+  .bind<ApiClient>(Types.ApiClient)
+  .toDynamicValue(() => new ApiClient({}));
+ioContainer.bind<HeaderApi>(Types.HeaderApi).to(HeaderApi);
+
+export default ioContainer;
