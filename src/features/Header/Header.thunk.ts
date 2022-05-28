f4a4c33abb057ca629728aee5c9d86cb616ad7ac
4sploit <4splo17@gmail.com>
2022-05-28 23:40:06 +0300

    create header thunk

---
+++ src/features/Header/Header.thunk.ts
@@ -0,0 +1,20 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ioContainer } from "config";
+import { Types } from "config/ioc";
+import HeaderApi from "features/Header/Header.api";
+
+const getHeaderData = createAsyncThunk(
+  "header/getHeaderData",
+  async (_, { rejectWithValue }) => {
+    const headerApi = ioContainer.get<HeaderApi>(Types.HeaderApi);
+    const response = await headerApi.getHeaderData();
+
+    if (response.isSuccess) {
+      return response.data;
+    } else {
+      return rejectWithValue(response.error);
+    }
+  }
+);
+
+export default getHeaderData;
