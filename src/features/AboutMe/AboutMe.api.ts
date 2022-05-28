1494e68dd844c19a9673794d8d3e4963edca5b91
4sploit <4splo17@gmail.com>
2022-05-29 01:53:04 +0300

    create about api

---
+++ src/features/AboutMe/AboutMe.api.ts
@@ -0,0 +1,17 @@
+import { ApiClient } from "common/clients";
+import { AboutData } from "common/models";
+import { ApiConstants } from "features/AboutMe/AboutMe.consts";
+
+class AboutApi {
+  constructor(private _apiClient: ApiClient) {}
+
+  getAboutData = async () => {
+    const response = await this._apiClient.get<AboutData>(
+      ApiConstants.AboutDBFile
+    );
+
+    return response;
+  };
+}
+
+export default AboutApi;
