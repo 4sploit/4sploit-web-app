daf7eb5804b757b8f79f5d8701265677663bdcc5
4sploit <4splo17@gmail.com>
2022-05-28 23:27:05 +0300

    create header api

---
+++ src/features/Header/Header.api.ts
@@ -0,0 +1,17 @@
+import { ApiClient } from "common/clients";
+import { HeaderData } from "common/models";
+import { ApiConstants } from "features/Header/Header.consts";
+
+class HeaderApi {
+  constructor(private _apiClient: ApiClient) {}
+
+  getHeaderData = async () => {
+    const response = await this._apiClient.get<HeaderData>(
+      ApiConstants.HeaderDBFile
+    );
+
+    return response;
+  };
+}
+
+export default HeaderApi;
