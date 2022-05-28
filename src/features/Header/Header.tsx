e0a1ce92b48aac6f36429cc45f28ec87a15869bb
4sploit <4splo17@gmail.com>
2022-05-28 23:50:58 +0300

    modify header feature to fetch it's data from api

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,16 +1,29 @@
-import { FC } from "react";
-import { useAppSelector } from "store";
+import { FC, useEffect } from "react";
+import { useTypedDispatch, useTypedSelector } from "store";
 import { NavBar } from "common/components/NavBar";
 import { HeaderProps, defaultProps } from "./Header.props";
 import { StyledHeader } from "./Header.style";
+import { ApiStatus } from "common/constants";
+import { Loader } from "common/components";
+import getHeaderData from "features/Header/Header.thunk";
 
 const Header: FC<HeaderProps> = () => {
-  const headerStore = useAppSelector((state) => state.header);
-  const logo = headerStore.logo;
+  const { data, status } = useTypedSelector((state) => state.header);
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    if (status == ApiStatus.Idle) {
+      dispatch(getHeaderData());
+    }
+  }, [status, dispatch]);
 
   return (
     <StyledHeader>
-      <NavBar logo={logo} />
+      {status == ApiStatus.Loading ? (
+        <Loader />
+      ) : (
+        data && <NavBar logo={data.logo} links={data.links} />
+      )}
     </StyledHeader>
   );
 };
