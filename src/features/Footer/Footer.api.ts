ba0f6063d0f53804ce9e36950a19913b078997b5
4sploit <4splo17@gmail.com>
2022-05-29 00:52:04 +0300

    create footer api

---
+++ src/features/Footer/Footer.api.ts
@@ -0,0 +1,17 @@
+import { ApiClient } from "common/clients";
+import { FooterData } from "common/models";
+import { ApiConstants } from "features/Footer/Footer.consts";
+
+class FooterApi {
+  constructor(private _apiClient: ApiClient) {}
+
+  getFooterData = async () => {
+    const response = await this._apiClient.get<FooterData>(
+      ApiConstants.FooterDBFile
+    );
+
+    return response;
+  };
+}
+
+export default FooterApi;
